e0dc21ea09928157ec0a220f20c1d17645abaaba
Elton Bahia <eltonbahiabc@gmail.com>
2017-05-15 21:12:21 -0400

    #Refactoring

--- controllers/config/server/server.js
+++ controllers/config/server/server.js
@@ -1,30 +1,30 @@
-const file = require('fs');
+// const file = require('fs');
 const app = require('../../../app');
 const serverCors = require('./cors');
 
 
 module.exports = function (app) {
 
-    // const port = process.env.PORT || 9080;
-    //
-    // app.listen(port, function () {
-    //
-    //     console.log('Server listening : ' + port)
-    // });
-    //
-    // app.use(serverCors);
+    const port = process.env.PORT || 9080;
 
-    file.readFile('././data/srv_nodejs', (err, data) => {
+    app.listen(port, function () {
 
-        if (err) throw err;
-
-        const port = process.env.PORT || parseInt(data);
-
-        app.listen(port, function () {
-
-            console.log('Server listening : ' + port)
-        });
-
-        app.use(serverCors);
+        console.log('Server listening : ' + port)
     });
+
+    app.use(serverCors);
+
+    // file.readFile('././data/srv_nodejs', (err, data) => {
+    //
+    //     if (err) throw err;
+    //
+    //     const port = process.env.PORT || parseInt(data);
+    //
+    //     app.listen(port, function () {
+    //
+    //         console.log('Server listening : ' + port)
+    //     });
+    //
+    //     app.use(serverCors);
+    // });
 };
