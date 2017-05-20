4b785ed7578bdf42b2e6894ad2e8313f59009578
Elton Bahia <eltonbahiabc@gmail.com>
2017-05-20 13:26:24 -0400

    #Refactoring

--- controllers/config/database/base.js
+++ controllers/config/database/base.js
@@ -2,11 +2,8 @@ const file = require('fs');
 const mongoose = require('mongoose');
 
 file.readFile('././data/srv_mongodb', (err, data) => {
-
     if (err) throw err;
-
     mongoose.connect(data, function () {
-
         console.log('Server MongoDB   : ' + data)
     });
 });
--- models/dao/dskcloud/dskcloud.js
+++ models/dao/dskcloud/dskcloud.js
@@ -6,9 +6,9 @@ const DesktopCloudSchema = new mongoose.Schema ({
     dsk_password: {type: String, required: true},
     dsk_address: {type: String, required: true, lowercase: true},
     dsk_port: {type: String, required: true},
-    dsk_data_registration: {type: Date, default: Date.now(), required: true},
-    dsk_status: {type: String, required: true, enum: ['E', 'D'], default: 'E', uppercase: true} //E: Enable, D: Disable
+    dsk_system: {type: String, required: true, uppercase: true},
+    dsk_status: {type: String, required: true, enum: ['E', 'D'], default: 'E', uppercase: true},
+    dsk_data_registration: {type: Date, default: Date.now(), required: true}
 });
-
 mongoose.model('dskcloud', DesktopCloudSchema);
 module.exports = mongoose.model('dskcloud');
